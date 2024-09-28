3466324d1f5eef5582818319464eb22964488e5f
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2024-09-28 14:51:30 -0500

    Ya se escribio el codigo relacionado con los comentarios y con los tipos de datos.

---
+++ src/Semana3.java
@@ -0,0 +1,24 @@
+public class Semana3 {
+    public static void main(String[] args) {
+        // Comentarios
+        // Cualquier cosa
+
+        /* Cualquier cosa
+           y otra cosa */
+
+        // Tipos de Datos
+        short numeroEntero1 = 1;
+        byte numeroEntero2 = 5;
+        int numeroEntero3 = 10; // Usen este
+        long numeroEntero4 = 100;
+
+        float numeroDecimal1 = 3.14f;
+        double numeroDecimal2 = 3.14; // Usen este
+
+        char letra = 'A';
+        String texto = "Hablen, pelaos!";
+
+        boolean esVerdadero = true;
+
+    }
+}
